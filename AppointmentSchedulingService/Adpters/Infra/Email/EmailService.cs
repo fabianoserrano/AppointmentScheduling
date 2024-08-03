e4347a37edcbd3bcd06b18ded208295e5f8e2b45
iderley <iderley@hotmail.com>
2024-08-02 21:13:17 -0300

    Ajuste e-mail

--- AppointmentSchedulingService/Adpters/Infra/Email/EmailService.cs
+++ AppointmentSchedulingService/Adpters/Infra/Email/EmailService.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Email.Ports;
 using Domain.Security;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace Infra.Email
 {
@@ -17,36 +18,71 @@ namespace Infra.Email
         {
             try
             {
-                MailAddress from = new MailAddress(_emailConfigurations.FromAddress, "Health&Med");
-                MailAddress to = new MailAddress(email.Appointment.Doctor.Email, email.Appointment.Doctor.Name);
-                MailMessage mail = new MailMessage(from, to);
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Health&Med", _emailConfigurations.Username));
+                message.To.Add(new MailboxAddress(email.Appointment.Doctor.Name, email.Appointment.Doctor.Email));
+                message.Subject = "Health&Med - Nova consulta agendada";
 
-                mail.Subject = "Health&Med - Nova consulta agendada";
-                mail.SubjectEncoding = System.Text.Encoding.UTF8;
-
-                mail.Body = @$"
+                message.Body = new TextPart("html")
+                {
+                    Text = @$"
 Olá, Dr. <b>{email.Appointment.Doctor.Name}</b>!<br>
 <br>
 Você tem uma nova consulta marcada!<br>
 Paciente: <b>{email.Appointment.Patient.Name}</b>.<br>
 Data e horário: <b>{email.Appointment.Date:dd/MM/yyyy}</b> às <b>{email.Appointment.Date:HH:mm}</b>.<br>
-                ";
+                "
+            };
 
-                mail.BodyEncoding = System.Text.Encoding.UTF8;
-                mail.IsBodyHtml = true;
+                using (var client = new SmtpClient())
+                {
+                    try
+                    {
+                        await client.ConnectAsync(_emailConfigurations.Server, _emailConfigurations.Port, _emailConfigurations.UseSsl);
+                        await client.AuthenticateAsync(_emailConfigurations.Username, _emailConfigurations.Password);
+                        await client.SendAsync(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error sending email: {ex.Message}");
+                    }
+                    finally
+                    {
+                        await client.DisconnectAsync(true);
+                    }
+                }
 
-                SmtpClient smtpClient = new SmtpClient(_emailConfigurations.Server, _emailConfigurations.Port);
-                smtpClient.UseDefaultCredentials = false;
-                System.Net.NetworkCredential basicAuthenticationInfo = new
-                   System.Net.NetworkCredential(_emailConfigurations.Username, _emailConfigurations.Password);
-                smtpClient.Credentials = basicAuthenticationInfo;
-                smtpClient.Send(mail);
+
+//                MailAddress from = new MailAddress(_emailConfigurations.FromAddress, "Health&Med");
+//                MailAddress to = new MailAddress(email.Appointment.Doctor.Email, email.Appointment.Doctor.Name);
+//                MailMessage mail = new MailMessage(from, to);
+
+//                mail.Subject = "Health&Med - Nova consulta agendada";
+//                mail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+//                mail.Body = @$"
+//Olá, Dr. <b>{email.Appointment.Doctor.Name}</b>!<br>
+//<br>
+//Você tem uma nova consulta marcada!<br>
+//Paciente: <b>{email.Appointment.Patient.Name}</b>.<br>
+//Data e horário: <b>{email.Appointment.Date:dd/MM/yyyy}</b> às <b>{email.Appointment.Date:HH:mm}</b>.<br>
+//                ";
+
+//                mail.BodyEncoding = System.Text.Encoding.UTF8;
+//                mail.IsBodyHtml = true;
+
+//                SmtpClient smtpClient = new SmtpClient(_emailConfigurations.Server, _emailConfigurations.Port);
+//                smtpClient.UseDefaultCredentials = false;
+//                System.Net.NetworkCredential basicAuthenticationInfo = new
+//                   System.Net.NetworkCredential(_emailConfigurations.Username, _emailConfigurations.Password);
+//                smtpClient.Credentials = basicAuthenticationInfo;
+//                smtpClient.Send(mail);
             }
-            catch (SmtpException ex)
-            {
-                throw new ApplicationException
-                  ("SmtpException has occured: " + ex.Message);
-            }
+            //catch (SmtpException ex)
+            //{
+            //    throw new ApplicationException
+            //      ("SmtpException has occured: " + ex.Message);
+            //}
             catch (Exception ex)
             {
                 throw ex;
--- AppointmentSchedulingService/Core/Domain/Security/EmailConfigurations.cs
+++ AppointmentSchedulingService/Core/Domain/Security/EmailConfigurations.cs
@@ -4,6 +4,7 @@
     {
         public string Server { get; set; }
         public int Port { get; set; }
+        public bool UseSsl { get; set; }
         public string FromAddress { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
